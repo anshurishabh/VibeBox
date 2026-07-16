0f4c520e9bdff879a89ef55b9a7097060d951b29
Anshu Kumar Rishabh <anshukr5000t@gmail.com>
2026-07-16 21:43:23 +0530

    Final build fix for static bailout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // <-- Yeh line yahan add karni hai
   images: {
     domains: ["is1-ssl.mzstatic.com", "is2-ssl.mzstatic.com", "is3-ssl.mzstatic.com", "is4-ssl.mzstatic.com", "is5-ssl.mzstatic.com"],
   },
